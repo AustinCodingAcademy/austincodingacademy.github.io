977436dd0ceb48edc754a14ec4b1b5265b080556
Kevin Colten <kevincolten@gmail.com>
2017-01-19 12:42:27 -0600

    finished transfer

--- _javascripts/app.js
+++ _javascripts/app.js
@@ -2,7 +2,27 @@ window.$ = window.jQuery = require('jquery');
 require('bootstrap.button');
 require('bootstrap.collapse');
 require('bootstrap.dropdown');
+require('bootstrap.modal');
 require('bootstrap.transition');
 require('slick-carousel');
 require('typed.js');
-require('jquery-form');
+
+$(function() {
+  if ($('.typed').length) {
+    $('.typed').each(function() {
+      $(this).typed({
+        typeSpeed: 10,
+        startDelay: 0,
+        backSpeed: 10,
+        backDelay: 3000,
+        loop: true,
+        showCursor: true,
+        cursor: "|",
+        stringsElement: $(this).find('.typed-strings')
+      });
+    });
+    var $cursor = $('.typed-cursor:nth-child(2)');
+    $cursor.prev().insertBefore($cursor);
+    $cursor.addClass('pull-right');
+  }
+});
