d310262975814bd6f28488cbd0ec05c473a414a4
Kevin Colten <kevincolten@gmail.com>
2018-04-14 01:42:42 -0500

    typo

--- generateScripts.js
+++ generateScripts.js
@@ -38,7 +38,7 @@ classics.concat(remotes).forEach(key => {
   packageJson.scripts[`sass-${key}`] = `node-sass --output-style compressed -o assets/stylesheets/ _sass/${key}.scss`;
   packageJson.scripts[`build-${key}`] = `bundle exec jekyll build --config _configs/_config_${key}.yml,_configs/_config.yml`;
   packageJson.scripts[`sitemap-${key}`] = `npm run sitemap-generator && sed -i 's|http://127.0.0.1:8080|https://${key}codingacademy.com|g' _site/sitemap.xml`;
-  packageJson.scripts[`deploy-${key}`] = `echo '${key}codingacademy.com' >> _site / CNAME && gh - pages--repo 'git@github.com:AustinCodingAcademy/${key}codingacademy.com.git' -d ./_site`;
+  packageJson.scripts[`deploy-${key}`] = `echo '${key}codingacademy.com' >> _site/CNAME && gh-pages --repo 'git@github.com:AustinCodingAcademy/${key}codingacademy.com.git' -d ./_site`;
 });
 
 fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
