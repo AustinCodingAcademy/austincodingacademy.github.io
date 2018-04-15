86284ca4ca77901b870eee03ddc17d359a0ebf5a
Kevin Colten <kevincolten@gmail.com>
2018-04-14 23:17:39 -0500

    deploy

--- deploy.js
+++ deploy.js
@@ -3,8 +3,8 @@ const packageJson = require('./package.json');
 
 // austin must go last since the remotes use its favicon
 ['dallas', 'houstontx', 'sanantonio', 'austin'].forEach(key => {
-  packageJson.scripts['deploy-classics'] = `${packageJson.scripts['deploy-classics']} && KEY=${key} npm run favicon && KEY=${key} npm run sass && npm run sass-include && KEY=${key} npm run build && KEY=${key} npm run sitemap && KEY=${key} npm run deploy && npm run clear-cache`;
-  packageJson.scripts['deploy-classics-preview'] = `${packageJson.scripts['deploy-classics-preview']} && KEY=${key} npm run favicon && KEY=${key} npm run build && KEY=${key} npm run sitemap && PREVIEW='preview.' KEY=${key} npm run deploy && npm run clear-cache`;
+  packageJson.scripts['deploy-classics'] = `${packageJson.scripts['deploy-classics']} && KEY=${key} npm run favicon && KEY=${key} npm run sass && npm run sass-include && KEY=${key} npm run build && KEY=${key} npm run sitemap && KEY=${key} npm run cname && KEY=${key} npm run deploy && npm run clear-cache`;
+  packageJson.scripts['deploy-classics-preview'] = `${packageJson.scripts['deploy-classics-preview']} && KEY=${key} npm run favicon && KEY=${key} npm run build && KEY=${key} npm run sitemap && && KEY=${key} npm run cname && PREVIEW='preview.' KEY=${key} npm run deploy && npm run clear-cache`;
 });
 
 [
@@ -27,7 +27,7 @@ const packageJson = require('./package.json');
   'texas',
   'vancouver'
 ].forEach(key => {
-  packageJson.scripts['deploy-remotes'] = `${packageJson.scripts['deploy-remotes']} && KEY=${key} npm run build && KEY=${key} npm run sitemap && KEY=${key} npm run deploy && npm run clear-cache`;
+  packageJson.scripts['deploy-remotes'] = `${packageJson.scripts['deploy-remotes']} && KEY=${key} npm run build && KEY=${key} npm run sitemap && KEY=${key} npm run cname && KEY=${key} npm run deploy && npm run clear-cache`;
 });
 
 fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
