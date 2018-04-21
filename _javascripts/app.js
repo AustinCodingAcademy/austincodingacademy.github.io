d3aa362f1b5c850f9022bc5976d74514e0dd811d
Kevin Colten <kevincolten@gmail.com>
2018-04-21 17:57:41 -0500

    tracks

--- _javascripts/app.js
+++ _javascripts/app.js
@@ -46,7 +46,7 @@ $(function() {
           <br/>
           ${moment.utc(dates['Austin']).format('ddd, MMM Do, YYYY')}
           <br/>
-          <small>North / <a href="/remote">Remote</a></small>
+          <small>North</small>
           <br />
           ${moment.utc(dates['North Austin']).format('ddd, MMM Do, YYYY')}
         `);
@@ -88,5 +88,10 @@ $(function() {
         $('#upcoming-events').html($upcomingEvents);
       }
     });
+  }
+
+  if ($('#scrollspy').length) {
+    $('body').scrollspy({ target: '#scrollspy' });
+    $('.nav-wrapper').height($('[data-spy="affix"]').height());
   }
 });
