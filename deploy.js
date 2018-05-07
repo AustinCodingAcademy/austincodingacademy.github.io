0bc69bd6bfa259fd9d65c7cf7d65fcbb4eb0caf0
Kevin Colten <kevincolten@gmail.com>
2018-05-07 00:09:05 -0500

    playing with deploy

--- deploy.js
+++ deploy.js
@@ -2,13 +2,6 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 const packageJson = require('./package.json');
 
-
-// austin must go last since the remotes use its favicon
-['dallas', 'houstontx', 'sanantonio', 'austin'].forEach(key => {
-  // packageJson.scripts['deploy-classics'] = `${packageJson.scripts['deploy-classics']} && KEY=${key} npm run favicon && KEY=${key} npm run sass && npm run sass-include && KEY=${key} npm run build && KEY=${key} npm run sitemap && KEY=${key} npm run cname && KEY=${key} npm run deploy && npm run clear-cache`;
-  // packageJson.scripts['deploy-classics-preview'] = `${packageJson.scripts['deploy-classics-preview']} && KEY=${key} npm run favicon && KEY=${key} npm run build && KEY=${key} npm run sitemap && && KEY=${key} npm run cname && PREVIEW='preview.' KEY=${key} npm run deploy && npm run clear-cache`;
-});
-
 [
   'austin',
   'boston',
@@ -33,9 +26,9 @@ const packageJson = require('./package.json');
   'texas',
   'vancouver'
 ].forEach(key => {
-  // packageJson.scripts['deploy-remotes'] = `${packageJson.scripts['deploy-remotes']} && KEY=${key} npm run sass && npm run sass-include && KEY=${key} npm run build && KEY=${key} npm run sitemap && KEY=${key} npm run cname && KEY=${key} npm run deploy && npm run clear-cache`;
   const config = yaml.safeLoad(fs.readFileSync(`./_configs/_config_${key}.yml`, 'utf8'));
   fs.writeFileSync(`./_sass/${key}.scss`, `$brand-success: ${config.color} !default; @import 'app';`)
+  packageJson.scripts['deploy'] += `KEY=${key} npm run sass && npm run sass-include && KEY=${key} npm run build && KEY=${key} npm run favicon && KEY=${key} npm run sitemap && KEY=${key} npm run cname && npm run clear-cache `;
 });
 
 fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
