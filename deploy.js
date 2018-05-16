d7905c2635b727695ecf807598c7efb1bf0233b5
Kevin Colten <kevincolten@gmail.com>
2018-05-16 14:25:30 -0500

    fixed repos, added success-light colot

--- deploy.js
+++ deploy.js
@@ -30,9 +30,10 @@ const packageJson = require('./package.json');
 ['austin', 'dallas', 'houstontx','sanantonio'].forEach(key => buildScript(key, 'deploy-preview'));
 
 function buildScript(key, deployScript) {
+  const preview = deployScript.includes('preview') ? 'preview.' : '';
   const config = yaml.safeLoad(fs.readFileSync(`./_configs/_config_${key}.yml`, 'utf8'));
-  fs.writeFileSync(`./_sass/${key}.scss`, `$brand-success: ${config.color} !default; @import 'app';`)
-  packageJson.scripts[deployScript] += ` && KEY=${key} yarn sass && yarn sass-include && KEY=${key} yarn build && rsvg-convert -h 100 _site/logo-white.svg > logo-white.png && KEY=${key} yarn favicon && KEY=${key} yarn build && KEY=${key} yarn sitemap && KEY=${key} yarn cname && KEY=${key} yarn repo && yarn clear-cache`;
+  fs.writeFileSync(`./_sass/${key}.scss`, `$brand-success: ${config.color} !default; $brand-success-light: ${config.lighter_color} !default; @import 'app';`);
+  packageJson.scripts[deployScript] += ` && KEY=${key} yarn sass && yarn sass-include && KEY=${key} yarn build && rsvg-convert -h 100 _site/logo-white.svg > logo-white.png && KEY=${key} yarn favicon && KEY=${key} yarn build && KEY=${key} yarn sitemap && KEY=${key} yarn cname && PREVIEW=${preview} KEY=${key} yarn repo && yarn clear-cache`;
 }
 
 fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
