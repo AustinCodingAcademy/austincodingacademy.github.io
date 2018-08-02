9892676adf0c83085a91d3569e3cc2580940a51e
Kevin Colten <kevincolten@gmail.com>
2018-08-02 10:45:38 -0500

    pa11y

--- accessibility.js
+++ accessibility.js
@@ -22,16 +22,10 @@ const results = []
 
 async function runPa11y() {
   try {
-      await Promise.all(routes.map(async route => {
-        results.push(await pa11y(`${base}${route}`, { standard: 'WCAG2AA' }));
-        console.log(JSON.stringify(results, null, 2));
-      }));
-      // const combinedResults = {}
-      // htmlReporter.results(results).then(html => {
-      //   const path = './tmp/accessibility.html';
-      //   fs.writeFileSync(path, html);
-      //   open(path);
-      // });
+    await Promise.all(routes.map(async route => {
+      results.push(await pa11y(`${base}${route}`, { standard: 'WCAG2AAA' }));
+      console.error(JSON.stringify(results, null, 2));
+    }));
   } catch (error) {
       console.error(error)
   }
